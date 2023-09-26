d9bc10ebfaf3c8dce2fd59ab2efc8301a7652fb0
Sikandar <sikandar.ramzan@codeninjaconsulting.com>
2023-09-26 16:04:30 +0500

    Revert "added CSV_Modifier_Client + removed Lambda proj cuz of csproj errors"
    This reverts commit 97b5416c699e6d0273f118a9c644bbe26447da90.

--- CSV_Modifier_Client/Models/HelloUserModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CSV_Modifier_Client.Models
-{
-    public class HelloUserModel
-    {
-        public string Token { get; set; } = string.Empty;
-    }
-}
--- CSV_Modifier_Client/Models/UploadViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CSV_Modifier_Client.Models
-{
-    public class UploadViewModel
-    {
-        public string Message { get; set; } = string.Empty;
-    }
-}
