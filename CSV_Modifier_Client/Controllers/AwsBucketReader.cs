97b5416c699e6d0273f118a9c644bbe26447da90
Sikandar <sikandar.ramzan@codeninjaconsulting.com>
2023-09-26 16:03:42 +0500

    added CSV_Modifier_Client + removed Lambda proj cuz of csproj errors

---
+++ CSV_Modifier_Client/Controllers/AwsBucketReader.cs
@@ -0,0 +1,78 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using CSV_Modifier_Client.Models;
+using CSV_Modifier_Client.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_Modifier_Client.Controllers
+{
+    public class AwsBucketReader : Controller
+    {
+        private readonly string bucketName = "csv-files-s3-bucket";
+
+        //fetching this file (object) from s3 bucket
+        private readonly string objectKey = "CommaSeparatedFile.csv";
+        private readonly AwsSecretsService _awsSecretsService;
+
+        public AwsBucketReader(AwsSecretsService awsSecretsService)
+        {
+            _awsSecretsService = awsSecretsService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //super user manager => superman
+            string superman_access_key = await _awsSecretsService.GetAwsSecret("superman_access_key");
+            string superman_secret_superman_access_key = await _awsSecretsService.GetAwsSecret("superman_secret_access_key");
+            try
+            {
+                using var s3Client = new AmazonS3Client(superman_access_key, superman_secret_superman_access_key);
+
+                var getObjectRequest = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = objectKey
+                };
+
+                using var response = await s3Client.GetObjectAsync(getObjectRequest);
+
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    using var reader = new StreamReader(response.ResponseStream);
+                    var csvModels = new List<CsvDataModel>();
+
+                    string? line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        var rowData = line.Split(',');
+                        if (rowData.Length >= 3)
+                        {
+                            var model = new CsvDataModel
+                            {
+                                ID = Convert.ToInt32(rowData[0]),
+                                Name = rowData[1],
+                                TechStack = rowData[2]
+                            };
+                            csvModels.Add(model);
+                        }
+                    }
+
+                    return View("Index", csvModels);
+                }
+                else
+                {
+                    return Content("Failed to retrieve CSV file from S3");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CSV_Modifier_Client/Controllers/HelloUserController.cs
@@ -0,0 +1,24 @@
+﻿using CSV_Modifier_Client.Models;
+using System.Diagnostics;
+using CSV_Modifier_Client.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSV_Modifier_Client.Controllers
+{
+    public class HelloUserController : Controller
+    {
+        private readonly AwsSecretsService _awsSecretsService;
+
+        public HelloUserController(AwsSecretsService awsSecretsService)
+        {
+            _awsSecretsService = awsSecretsService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            const string secretName = "UserName";
+            string awsSecret = await _awsSecretsService.GetAwsSecret(secretName);
+
+            return View(new HelloUserModel { Token=awsSecret });
+        }
+    }
+}
