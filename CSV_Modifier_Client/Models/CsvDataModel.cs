c93b8e1dd13c6e5ba4e924e0d310253bd853fce9
Sikandar Ramzan <34787764+sikandar-ramzan@users.noreply.github.com>
2023-09-26 18:40:12 +0500

    Revert "init aws sns & sqs project"

--- CSV_Modifier_Client/Models/CsvDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSV_Modifier_Client.Models
-{
-    public class CsvDataModel
-    {
-        public int ID { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string TechStack { get; set; } = string.Empty;
-    }
-}
